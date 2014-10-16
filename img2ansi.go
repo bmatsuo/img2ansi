ebdfa2a71b8cc687ba0edf644513221664d546ac
Bryan Matsuo <bryan.matsuo@gmail.com>
2014-10-15 20:19:47 -0700

    use all cpus in image resizing

--- img2ansi.go
+++ img2ansi.go
@@ -12,6 +12,7 @@ import (
 	"log"
 	"math"
 	"os"
+	"runtime"
 	"runtime/pprof"
 	"strconv"
 
@@ -21,6 +22,7 @@ import (
 const ANSIClear = "\033[0m"
 
 func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 	log.SetFlags(0)
 }
 
