a523c15dc3c8f61f2e781f1867fc92afc535a493
Bryan Matsuo <bryan.matsuo@gmail.com>
2015-06-19 21:15:37 -0700

    support for animated gifs

--- img2ansi.go
+++ img2ansi.go
@@ -1,11 +1,21 @@
+/*
+	Command img2ansi renders images for a terminal using ANSI color codes.
+
+		img2ansi motd.png
+		img2ansi -animate -repeat=100 -width=78 https://i.imgur.com/872FDBm.gif
+		img2ansi -h
+*/
 package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
+	"fmt"
 	"image"
 	"image/color"
-	_ "image/gif"
+	"image/draw"
+	"image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 	"io"
@@ -33,16 +43,28 @@ func init() {
 }
 
 func main() {
+	fopts := new(FrameOptions)
+
 	cpuprofile := flag.String("cpuprofile", "", "path of pprof CPU profile output")
 	width := flag.Int("width", 0, "desired width in terminal columns")
-	pad := flag.Bool("pad", false, "pad output on the left with whitespace")
 	paletteName := flag.String("color", "256", "color palette (8, 256, gray, ...)")
 	fontAspect := flag.Float64("fontaspect", 0.5, "aspect ratio (width/height)")
 	alphaThreshold := flag.Float64("alphamin", 1.0, "transparency threshold")
 	useStdin := flag.Bool("stdin", false, "read image data from stdin")
+	flag.StringVar(&fopts.Pad, "pad", " ", "pad output on the left with whitespace")
+	flag.BoolVar(&fopts.Animate, "animate", false, "animate images")
+	flag.IntVar(&fopts.Repeat, "repeat", 0, "number of animated loops")
 	flag.Parse()
+	if *useStdin && flag.NArg() > 0 {
+		log.Fatal("no arguments are expected when -stdin provided")
+	}
 
 	AlphaThreshold = uint32(*alphaThreshold * float64(0xffff))
+
+	palette := ansiPalettes[*paletteName]
+	if palette == nil {
+		log.Fatalf("color palette not one of %q", ANSIPalettes())
+	}
 
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
@@ -53,69 +75,166 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	palette := ansiPalettes[*paletteName]
-	if palette == nil {
-		log.Fatalf("color palette not one of %q", ANSIPalettes())
-	}
-
-	var img image.Image
+	var imgs []image.Image
 	var err error
-	if *useStdin {
-		img, _, err = image.Decode(os.Stdin)
+	if *useStdin || flag.NArg() == 0 {
+		imgs, err = readFrames(os.Stdin)
 	} else {
-		if flag.NArg() < 1 {
-			log.Fatal("missing filename")
+		for _, filename := range flag.Args() {
+			var fimgs []image.Image
+			fimgs, err = readFramesFile(filename)
+			imgs = append(imgs, fimgs...)
 		}
-		if flag.NArg() > 1 {
-			log.Fatal("unexpected arguments")
-		}
-		filename := flag.Arg(0)
-		img, _, err = readImage(filename)
 	}
 	if err != nil {
 		log.Fatalf("image: %v", err)
 	}
 
 	// resize img to the proper width and aspect ratio
-	size := img.Bounds().Size()
-	if *width > 0 {
-		size = sizeWidth(size, *width, *fontAspect)
-	} else {
-		size = sizeNormal(size, *fontAspect)
-	}
-	if size != img.Bounds().Size() {
-		img = resize.Resize(uint(size.X), uint(size.Y), img, 0)
+	for i, img := range imgs {
+		size := img.Bounds().Size()
+		if *width > 0 {
+			size = sizeWidth(size, *width, *fontAspect)
+		} else {
+			size = sizeNormal(size, *fontAspect)
+		}
+		if size != img.Bounds().Size() {
+			img = resize.Resize(uint(size.X), uint(size.Y), img, 0)
+		}
+		imgs[i] = img
 	}
 
-	err = writePixelsANSI(os.Stdout, img, palette, *pad)
+	err = writeANSIFramePixels(os.Stdout, imgs, palette, fopts)
 	if err != nil {
 		log.Fatalf("write: %v", err)
 	}
 }
 
+// FrameOptions describes how to render a sequence of frames in a terminal.
+type FrameOptions struct {
+	// Pad is a string prepended to each row of pixels.
+	Pad string
+
+	// Animate will animate the frames when true.  Animation is accomplished by
+	// emitting a control sequence to reset the cursor before rendering each
+	// frame.
+	Animate bool
+
+	// Repeat specifies the number of times to render the frame sequence.  If
+	// Repeat is zero the frames are rendered just once.  If Repeat is less
+	// than zero the frames are rendered indefinitely.
+	Repeat int
+}
+
+func writeANSIFramePixels(w io.Writer, imgs []image.Image, p ANSIPalette, opts *FrameOptions) error {
+	var rect image.Rectangle
+	animate := opts != nil && opts.Animate
+
+	loopn := 1
+	if opts != nil {
+		loopn += opts.Repeat
+	}
+
+	for loop := 0; loopn <= 0 || loop < loopn; loop++ {
+		for _, img := range imgs {
+			if animate {
+				up := rect.Size().Y
+				rect = img.Bounds()
+				if up > 0 {
+					fmt.Fprintf(w, "\033[%dA", up)
+				}
+			}
+			err := writeANSIPixels(w, img, p, opts.Pad)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 var lineBytes = []byte{'\n'}
 var spaceBytes = []byte{' '}
 
-func writePixelsANSI(w io.Writer, img image.Image, p ANSIPalette, pad bool) error {
+func writeANSIPixels(w io.Writer, img image.Image, p ANSIPalette, pad string) error {
 	wbuf := bufio.NewWriter(w)
+	writeansii := func() func(color string) {
+		var lastcolor string
+		return func(color string) {
+			if color != lastcolor {
+				lastcolor = color
+				wbuf.WriteString(color)
+			}
+		}
+	}()
 	rect := img.Bounds()
 	size := rect.Size()
 	for y := 0; y < size.Y; y++ {
-		if pad {
-			wbuf.Write(spaceBytes)
-		}
+		wbuf.WriteString(pad)
 		for x := 0; x < size.X; x++ {
 			color := img.At(rect.Min.X+x, rect.Min.Y+y)
-			wbuf.WriteString(p.ANSI(color))
+			writeansii(p.ANSI(color))
 			wbuf.Write(spaceBytes)
 		}
-		if pad {
-			wbuf.Write(spaceBytes)
-		}
-		wbuf.WriteString(ANSIClear)
+		wbuf.WriteString(pad)
+		writeansii(ANSIClear)
 		wbuf.Write(lineBytes)
 	}
 	return wbuf.Flush()
+}
+
+func readFramesFile(filename string) ([]image.Image, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return readFrames(f)
+}
+
+func readFrames(r io.Reader) ([]image.Image, error) {
+	var confbuf bytes.Buffer
+	_, format, err := image.DecodeConfig(io.TeeReader(r, &confbuf))
+	if err != nil {
+		return nil, err
+	}
+	r = io.MultiReader(&confbuf, r)
+	if format == "gif" {
+		return readFramesGIF(r)
+	}
+	img, _, err := image.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	return []image.Image{img}, nil
+}
+
+func readFramesGIF(r io.Reader) ([]image.Image, error) {
+	img, err := gif.DecodeAll(r)
+	if err != nil {
+		return nil, err
+	}
+	return framesGIF(img), nil
+}
+
+// framesGIF computes the raw frames of g by successively applying layers.
+func framesGIF(g *gif.GIF) []image.Image {
+	var imgs []image.Image
+	rect := g.Image[0].Rect
+	frame := image.NewRGBA64(rect)
+	for _, layer := range g.Image {
+		lrect := layer.Bounds()
+		draw.Draw(frame, lrect, layer, lrect.Min, draw.Over)
+
+		cp := new(image.RGBA64)
+		*cp = *frame
+		cp.Pix = make([]uint8, len(frame.Pix))
+		copy(cp.Pix, frame.Pix)
+
+		imgs = append(imgs, cp)
+	}
+	return imgs
 }
 
 // readImage reads an image.Image from a specified file.
